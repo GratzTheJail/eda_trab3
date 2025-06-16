4ea8390e39413b92c4a21ed881d874a7d44f6b9b
GratzTheJail <guigagratz@hotmail.com>
2025-06-16 09:31:33 -0300

    structs

--- grafos.h
+++ grafos.h
@@ -1,1 +1,14 @@
 // teste
+typedef struct _viz Viz;
+struct _viz {
+    int n;
+    float peso;
+    Viz* prox;
+};
+
+typedef struct _grafo Grafo;
+struct _grafo {
+    int nv; /* numero de nos ou vertices */
+    int na; /* numero de arestas */
+    Viz** viz; /* viz[i] aponta para a lista de arestas incidindo em i */
+};
