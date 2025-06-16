e7f690b09a334379d448ad9d0330ee458db34cc3
GratzTheJail <guigagratz@hotmail.com>
2025-06-16 10:21:41 -0300

    criavertice e inicializagrafo

--- grafos.c
+++ grafos.c
@@ -1,1 +1,37 @@
 //TESTE
+#ifndef GRAFOS
+#define GRAFOS
+#include "grafos.h"
+#endif
+#include <stdlib.h>
+
+Grafo* inicializaGrafo(){
+    Grafo* grafo = (Grafo*)malloc(sizeof(Grafo));
+    if(grafo == NULL)
+        exit(1);
+    grafo->viz = NULL;
+    return grafo;
+}
+
+int criaVertice(Grafo* grafo, int vizinhos[], int peso[], int tam){
+    grafo->nv++;
+    grafo->viz = postInsert(grafo->viz, grafo->nv);
+    Node* vAtual = findNode(grafo->viz, grafo->nv);
+    vAtual->obj = NULL;
+
+    if(tam == 0){
+        return  1;
+    }
+
+    for(int i = 0; i < tam; i++){
+        Viz* viz = (Viz*)malloc(sizeof(Viz));
+        if(viz == NULL) exit(1);
+
+        viz->peso = peso[i];
+        viz->n = vizinhos[i];
+
+        vAtual->obj = (void*)postInsert(vAtual->obj, countNodes(vAtual->obj));
+    }
+
+    return 1;
+}   
--- grafos.h
+++ grafos.h
@@ -1,14 +1,18 @@
+#ifndef LIST
+#define LIST
+#include "list/list.h"
+#endif
 // teste
 typedef struct _viz Viz;
 struct _viz {
     int n;
     float peso;
-    Viz* prox;
+    // Node* prox;
 };
 
 typedef struct _grafo Grafo;
 struct _grafo {
     int nv; /* numero de nos ou vertices */
     int na; /* numero de arestas */
-    Viz** viz; /* viz[i] aponta para a lista de arestas incidindo em i */
+    Node* viz; /* viz[i] aponta para a lista de arestas incidindo em i */
 };
